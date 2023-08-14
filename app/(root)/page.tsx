2ed22f9a78a7cc671a1e1a99beb6f02676b15615
maikolcw <whysoraccoony@gmail.com>
2023-08-14 16:45:30 -0700

    added our authorization via clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Treads my Thread Clone</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
