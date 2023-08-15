0c7eb6ec81e682ed24f353d13553759b7fd5701b
maikolcw <whysoraccoony@gmail.com>
2023-08-14 21:07:33 -0700

    added template page for onboarding route

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function Page() {
+    return (
+        <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+            <h1 className='head-text'>Onboarding</h1>
+        </main>
+    );
+}
+
+export default Page;
