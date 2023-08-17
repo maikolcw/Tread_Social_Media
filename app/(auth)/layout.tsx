25c2bacaeb429959bff1dc263834d2bb86d9bd62
maikolcw <whysoraccoony@gmail.com>
2023-08-16 20:54:21 -0700

    updated and fixed onboarding page, updated activity page, and added getActivity action

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,36 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import "../globals.css"
+import { dark } from "@clerk/themes"
+import { Metadata } from "next"
+import React from "react";
+import "../globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "Tread",
     description: "A Next.js 13 Meta Threads Spinoff"
 }
 
-const inter = Inter({ subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-    children }: {
-        children: React.ReactNode
-    }) {
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
-    <ClerkProvider>
-        <html lang="en">
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
-        </html>
-    </ClerkProvider>
-)
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
